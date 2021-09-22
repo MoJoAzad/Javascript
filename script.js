0d366db6dcab0f532cd7cb24f9b80ed9dfea74e8
Mohammed J Azad <87226195+MoJoAzad@users.noreply.github.com>
2021-09-22 15:35:52 -0700

    Add files via upload
    just some simple for loop exercises

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-
-function over(element){
-    element.play();
+// problem 1//
+for (var i=0;i<=20;i++){
+    if(i%2==0){
+        console.log(i)
+    }
 }
-
-function out(element){
-    element.pause();
+// problem 2//
+for (var i=100;i>=0;i--){
+    if(i%3==0){
+console.log(i)}
 }
+// problem 3//
+var arr=[4,2.5,1,-0.5,-2,-3.5];
+for(i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+// problem 4//
+var sum=0;
+for(i=0;i<101;i++){
+    sum+=i;
+}
+console.log(sum);
+
+// problems 5//
+var product=1;
+for (i=1;i<13;i++){
+    product*=i;
+}
+console.log(product);
